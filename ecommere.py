eb1458b2511ebb88764a7900305e8d1f1eb4c336
sumaika <sumaikafish@gmail.com>
2025-09-19 10:46:49 +0500

    Update ecommere.py

--- ecommere.py
+++ ecommere.py
@@ -24,7 +24,7 @@ if Ufile :
         df[column].hist(ax=ax, bins=20)
         ax.set_title(f"Histogram of {column}")
         st.pyplot(fig)
-     elif column == "order_date":
+    elif column == "order_date":
         df["order_date"] = pd.to_datetime(df["order_date"])
         monthly_orders = (
             df.groupby(df["order_date"].dt.to_period("M"))["order_id"]
@@ -46,5 +46,6 @@ if Ufile :
 
 
         
+
 
 
